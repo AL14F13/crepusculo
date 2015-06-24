10db88bf63a90211161c6fdd75b6ebae546714ab
AL14F13 <BotkkaBela24@gmail.com>
2015-06-24 11:13:19 -0500

    TRES
    ACCIONES

---
+++ js/acciones.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	$('#btnsaga').on('tap',function(){
+		navigator.vibrate(1000);
+		
+	});
+}); 
+});
+
